bc394d3d1a75100b58918811464816fac74f03e7
seungueonn <helloworldgcc@gmail.com>
2024-08-14 01:21:13 +0900

    [BOJ] 동전0 / 실버 4 / 성공

--- seungyeon/BAEKJOON/그리디/11047_동전0.py
+++ seungyeon/BAEKJOON/그리디/11047_동전0.py
@@ -1,13 +1,30 @@
-n,k=map(int,input().split())
-coins = []
+# n,k=map(int,input().split())
+# coins = []
+# for i in range(n):
+#     coins.append(int(input()))
+
+# cnt = 0
+# for i in range(n-1,-1,-1):
+    
+#     if coins[i] <= k :
+#         cnt += k // coins[i]
+#         k = k % coins[i]
+
+# print(cnt)
+
+import sys
+input=sys.stdin.readline
+
+n,k=map(int,input().split(" "))
+arr =[]
 for i in range(n):
-    coins.append(int(input()))
+    arr.append(int(input()))
 
 cnt = 0
+
 for i in range(n-1,-1,-1):
-    
-    if coins[i] <= k :
-        cnt += k // coins[i]
-        k = k % coins[i]
+    if k // arr[i] > 0:
+        cnt += k // arr[i]
+        k %= arr[i]
 
 print(cnt)
