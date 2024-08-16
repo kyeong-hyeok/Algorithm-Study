9fd1d4fd554e72f11876b0d7e67ea81fdd174d70
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2024-08-16 09:56:36 +0900

    [BOJ] 회사에 있는 사람 / 해시

--- seungyeon/BAEKJOON/해시/7785_회사에 있는 사람.py
+++ seungyeon/BAEKJOON/해시/7785_회사에 있는 사람.py
@@ -1,15 +1,30 @@
-import sys
-from collections import deque
-input=sys.stdin.readline
+# import sys
+# from collections import deque
+# input=sys.stdin.readline
 
-arr={}
-n = int(input())
+# arr={}
+# n = int(input())
 
+# for i in range(n):
+#     name,status=map(str,input().split())
+#     if status=='enter':
+#         arr[name]=True
+#     else:
+#         del arr[name]
+
+# print('\n'.join(sorted(arr,reverse=True)))
+
+
+n=int(input())
+arr=set()
 for i in range(n):
-    name,status=map(str,input().split())
-    if status=='enter':
-        arr[name]=True
-    else:
-        del arr[name]
+    name,status=input().split(" ")
 
-print('\n'.join(sorted(arr,reverse=True)))
+    if status == "enter":
+        arr.add(name)
+    else:
+        arr.remove(name) 
+
+arr=sorted(arr,reverse=True)
+for i in arr:
+    print(i)
