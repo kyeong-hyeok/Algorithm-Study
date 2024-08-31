dc221ec01f727936536f824f5ddf1ae39fcfe0c7
seungueonn <helloworldgcc@gmail.com>
2024-08-31 12:39:07 +0900

    [BOJ] 최소힙 / 실버2 / heapq

--- seungyeon/BAEKJOON/힙/1927_최소 힙.py
+++ seungyeon/BAEKJOON/힙/1927_최소 힙.py
@@ -1,16 +1,33 @@
-import sys
-import heapq
-input=sys.stdin.readline
+# import sys
+# import heapq
+# input=sys.stdin.readline
+
+# n = int(input())
+# arr=[]
+# for i in range(n):
+#     x = int(input())
+
+#     if x == 0:
+#            if not arr:
+#                 print(0)
+#            else:
+#                 print(heapq.heappop(arr))
+#     else:
+#         heapq.heappush(arr,x)
+
+import sys,heapq
+input = sys.stdin.readline
 
 n = int(input())
+
 arr=[]
 for i in range(n):
-    x = int(input())
+    k = int(input())
 
-    if x == 0:
-           if not arr:
-                print(0)
-           else:
-                print(heapq.heappop(arr))
+    if k == 0:
+        if not arr:
+            print(0)
+        else:
+            print(heapq.heappop(arr))
     else:
-        heapq.heappush(arr,x)
+        heapq.heappush(arr,k)
