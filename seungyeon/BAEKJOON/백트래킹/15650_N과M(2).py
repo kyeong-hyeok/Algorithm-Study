edac5b960cdb669124287bc8505a9b6addb62e67
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2024-09-03 08:50:03 +0900

    [BOJ] N과M(2) / 실버 3 / 백트래킹

--- seungyeon/BAEKJOON/백트래킹/15650_N과M(2).py
+++ seungyeon/BAEKJOON/백트래킹/15650_N과M(2).py
@@ -1,30 +1,58 @@
+# import sys
+# from itertools import combinations
+
+# input = sys.stdin.readline
+# n,m = map(int,input().split())
+
+# num = [i for i in range(1,n+1)]
+
+
+# # for i in combinations(num,m):
+# #     print(*i)
+
+
+# visited=[False]*(n+1)
+# arr = []
+
+# def dfs(depth):
+#     if depth == m:
+#         print(''.join(map(str,arr)))
+#         return
+    
+#     for i in range(1,n+1):
+#         if not visited[i]:
+#             visited[i] = True
+#             arr.append(i)
+#             dfs(depth+1)
+#             visited[i] = False
+#             arr.pop()
+
+# dfs(0)
+
 import sys
-from itertools import combinations
-
-input = sys.stdin.readline
-n,m = map(int,input().split())
-
-num = [i for i in range(1,n+1)]
+input=sys.stdin.readline
 
 
-# for i in combinations(num,m):
-#     print(*i)
+n,m=map(int,input().split())
 
-
-visited=[False]*(n+1)
+visited=[False] * (n+1)
 arr = []
+# 중복없이, 오름차순
+answer =[]
 
 def dfs(depth):
+
     if depth == m:
-        print(''.join(map(str,arr)))
-        return
+        print(*answer)
+        return 
     
     for i in range(1,n+1):
+        
         if not visited[i]:
             visited[i] = True
-            arr.append(i)
+            answer.append(i)
             dfs(depth+1)
-            visited[i] = False
-            arr.pop()
+            visited[i]=False
+            answer.pop()
 
 dfs(0)
