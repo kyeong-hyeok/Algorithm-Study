e8bb3ceb81b3ac08de1fe1da1645dd501872a518
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2024-09-03 08:41:35 +0900

    [BOJ] 단지번호 붙이기 / 실버 1 / BFS

--- seungyeon/BAEKJOON/BFS/2667_단지번호붙이기.py
+++ seungyeon/BAEKJOON/BFS/2667_단지번호붙이기.py
@@ -1,43 +1,92 @@
+# import sys
+# from collections import deque
+
+
+# n = int(input())
+# arr=[]
+# for i in range(n):
+#     arr.append(list(map(int,input())))
+
+# answer = []
+# dx=[1,-1,0,0]
+# dy=[0,0,1,-1]
+
+# def bfs(x,y):
+#     que = deque()
+#     cnt = 1
+
+#     que.append((x,y))
+
+#     while(que):
+#         x,y = que.popleft()
+#         arr[x][y] = 0
+
+#         for i in range(4):
+#             nx = x+dx[i]
+#             ny = y+dy[i]
+
+#             if 0<= nx < n and 0<= ny < n and arr[nx][ny] == 1:
+#                 que.append((nx,ny))
+#                 cnt += 1
+#                 arr[nx][ny] = 0
+
+#     answer.append(cnt)
+
+# for i in range(n):
+#     for j in range(n):
+#         if arr[i][j] == 1:
+#             bfs(i,j)
+
+# print(len(answer))
+# answer.sort()
+# for i in answer:
+#     print(i)
+
 import sys
 from collections import deque
 
+input=sys.stdin.readline
 
 n = int(input())
 arr=[]
 for i in range(n):
-    arr.append(list(map(int,input())))
+    arr.append(list(map(int,input().rstrip())))
 
-answer = []
-dx=[1,-1,0,0]
+
+dx=[-1,1,0,0]
 dy=[0,0,1,-1]
 
 def bfs(x,y):
-    que = deque()
-    cnt = 1
+    que=deque()
 
     que.append((x,y))
-
+    arr[x][y] = 0
+    cnt = 1
+    
+    
     while(que):
-        x,y = que.popleft()
-        arr[x][y] = 0
+        x,y=que.popleft()
 
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
+            nx=x+dx[i]
+            ny=y+dy[i]
 
-            if 0<= nx < n and 0<= ny < n and arr[nx][ny] == 1:
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if arr[nx][ny] == 1:
+                arr[nx][ny] = 0
                 que.append((nx,ny))
                 cnt += 1
-                arr[nx][ny] = 0
+    return cnt
 
-    answer.append(cnt)
 
+answer = []
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 1:
-            bfs(i,j)
+            answer.append(bfs(i,j))
 
-print(len(answer))
 answer.sort()
-for i in answer:
-    print(i)
+print(len(answer))
+for i in range(len(answer)):
+    print(answer[i])
