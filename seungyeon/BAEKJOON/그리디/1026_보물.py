7b636b7f8c28ea303688c847212960f7a7c23d56
seungueonn <helloworldgcc@gmail.com>
2024-09-04 00:25:48 +0900

    [BOJ] 보물 / 실버 4 / 그리디

--- seungyeon/BAEKJOON/그리디/1026_보물.py
+++ seungyeon/BAEKJOON/그리디/1026_보물.py
@@ -1,14 +1,30 @@
-import sys
+# import sys
+# input=sys.stdin.readline
+
+# n = int(input())
+# a=list(map(int,input().split()))
+# b=list(map(int,input().split()))
+
+# a.sort()
+# sum = 0
+
+# for i in range(n):
+#     sum += max(b)*a[i]
+#     b.remove(max(b))
+# print(sum)
+
+import sys,heapq
 input=sys.stdin.readline
+n=int(input())
+a = sorted(list(map(int,input().split())))
+b = list(map(int,input().split()))
 
-n = int(input())
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
+que=[]
 
-a.sort()
+for i in b:
+    heapq.heappush(que,-i)
 sum = 0
+for i in a:
+    sum += i * (-1 * heapq.heappop(que))
 
-for i in range(n):
-    sum += max(b)*a[i]
-    b.remove(max(b))
 print(sum)
