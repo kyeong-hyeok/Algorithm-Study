4f24f314e3cbeb54361343a4979843f71a391c66
seungueonn <jeongsy8928@soongsil.ac.kr>
2025-02-10 10:01:55 +0900

    [BOJ] DFS와 BFS / 실버 2 / DFS, BFS

--- seungyeon/BAEKJOON/DFS/1260_DFS와 BFS.py
+++ seungyeon/BAEKJOON/DFS/1260_DFS와 BFS.py
@@ -1,95 +1,163 @@
-# import sys
-# from collections import deque
-# input=sys.stdin.readline
+# # import sys
+# # from collections import deque
+# # input=sys.stdin.readline
 
-# def bfs(v):
-#     q = deque([v])
-#     visited[v] = True # 방문처리
-#     while q :
-#         v = q.popleft()
-#         print(v,end=" ")
-#         for i in range(1,n+1):
-#             if not visited[i] and arr[v][i]: # 방문하지 않았고, 간선이 있을 때
-#                 q.append(i)
-#                 visited[i] = True
+# # def bfs(v):
+# #     q = deque([v])
+# #     visited[v] = True # 방문처리
+# #     while q :
+# #         v = q.popleft()
+# #         print(v,end=" ")
+# #         for i in range(1,n+1):
+# #             if not visited[i] and arr[v][i]: # 방문하지 않았고, 간선이 있을 때
+# #                 q.append(i)
+# #                 visited[i] = True
 
-# def dfs(v):
-#     visited[v] = True
-#     print(v,end=" ")
-#     for i in range(1,n+1):
-#         if not visited[i] and arr[v][i]:
-#             dfs(i)
+# # def dfs(v):
+# #     visited[v] = True
+# #     print(v,end=" ")
+# #     for i in range(1,n+1):
+# #         if not visited[i] and arr[v][i]:
+# #             dfs(i)
 
-# n,m,v = map(int,input().split())
-# arr = [[False]*(n+1)  for _ in range(n+1)]
+# # n,m,v = map(int,input().split())
+# # arr = [[False]*(n+1)  for _ in range(n+1)]
 
-# for _ in range(m):
-#     a,b = map(int,input().split())
-#     arr[a][b] = True
-#     arr[b][a] = True
+# # for _ in range(m):
+# #     a,b = map(int,input().split())
+# #     arr[a][b] = True
+# #     arr[b][a] = True
 
-# visited = [False] * (n+1)
-# dfs(v)
-# print()
-# visited = [False]*(n+1)
-# bfs(v)
+# # visited = [False] * (n+1)
+# # dfs(v)
+# # print()
+# # visited = [False]*(n+1)
+# # bfs(v)
 
-# import sys
-# from collections import deque
+# # import sys
+# # from collections import deque
 
-# input=sys.stdin.readline
+# # input=sys.stdin.readline
 
-# n, m, v = map(int, sys.stdin.readline().split())
+# # n, m, v = map(int, sys.stdin.readline().split())
 
-# graph = [[] for _ in range(n+1)]
-# visited = [False] * (n+1)
+# # graph = [[] for _ in range(n+1)]
+# # visited = [False] * (n+1)
 
-# for i in range(m):
-#     x, y = map(int, input().split())
-#     graph[x].append(y)
-#     graph[y].append(x)
+# # for i in range(m):
+# #     x, y = map(int, input().split())
+# #     graph[x].append(y)
+# #     graph[y].append(x)
     
-# for i in range(n+1):
-#     graph[i].sort()
+# # for i in range(n+1):
+# #     graph[i].sort()
 
+
+# # def dfs(v):
+# #     visited[v] = True
+# #     print(v, end=' ')
+
+# #     for i in graph[v]:
+# #         if not visited[i]:
+# #             dfs(i)
+
+# # def bfs(v):
+# #     queue = deque([v])
+# #     visited[v] = True
+
+# #     while queue:
+# #         v = queue.popleft()
+# #         print(v, end=' ')
+
+# #         for i in graph[v]:
+# #             if not visited[i]:
+# #                 queue.append(i)
+# #                 visited[i] = True
+
+# # dfs(v)
+# # print()
+
+# # visited = [False] * (n+1)
+
+# # bfs(v)
+# # print()
+
+
+# import sys
+# from collections import deque
+# input=sys.stdin.readline
+# n,m,v=map(int,input().split())
+
+# arr=[[] for i in range(n+1)]
+# visited=[False]*(n+1)
+# for i in range(m):
+#     a,b=map(int,input().split())
+#     arr[a].append(b)
+#     arr[b].append(a)
+
+# for i in range(n+1):
+#     arr[i].sort()
 
 # def dfs(v):
 #     visited[v] = True
-#     print(v, end=' ')
+#     print(v,end=' ')
 
-#     for i in graph[v]:
+#     for i in arr[v]:
 #         if not visited[i]:
 #             dfs(i)
 
+
 # def bfs(v):
-#     queue = deque([v])
-#     visited[v] = True
+#     que=deque([v])
+#     visited[v] = True # 처음 시작 visied 변경 주의
+#     while(que):
+#         v = que.popleft()
+#         print(v,end=' ')
 
-#     while queue:
-#         v = queue.popleft()
-#         print(v, end=' ')
-
-#         for i in graph[v]:
+#         for i in arr[v]:
 #             if not visited[i]:
-#                 queue.append(i)
-#                 visited[i] = True
+#                 visited[i] = True # 재귀 하기 전 visied 변경
+#                 que.append(i)
+
+
 
 # dfs(v)
 # print()
-
-# visited = [False] * (n+1)
-
+# visited=[False]*(n+1)
 # bfs(v)
 # print()
+                
 
 
 import sys
 from collections import deque
 input=sys.stdin.readline
-n,m,v=map(int,input().split())
 
-arr=[[] for i in range(n+1)]
+def bfs(x):
+    que=deque()
+    que.append(x)
+    visited[x] = True
+    while que:
+        x = que.popleft()
+        print(x,end=' ')
+        for i in arr[x]:
+            if not visited[i]:
+                visited[i] = True
+                que.append(i)
+
+def dfs(x):
+
+    visited[x] = True 
+    print(x,end=' ')
+    for i in arr[x]:
+        if not visited[i]:
+            dfs(i)
+
+
+n,m,v=map(int,input().split())
 visited=[False]*(n+1)
+arr=[[] for i in range(n+1)]
+
 for i in range(m):
     a,b=map(int,input().split())
     arr[a].append(b)
@@ -98,32 +166,8 @@ for i in range(m):
 for i in range(n+1):
     arr[i].sort()
 
-def dfs(v):
-    visited[v] = True
-    print(v,end=' ')
-
-    for i in arr[v]:
-        if not visited[i]:
-            dfs(i)
-
-
-def bfs(v):
-    que=deque([v])
-    visited[v] = True # 처음 시작 visied 변경 주의
-    while(que):
-        v = que.popleft()
-        print(v,end=' ')
-
-        for i in arr[v]:
-            if not visited[i]:
-                visited[i] = True # 재귀 하기 전 visied 변경
-                que.append(i)
-
-
 
 dfs(v)
 print()
-visited=[False]*(n+1)
+visited = [False] * (n+1)
 bfs(v)
-print()
-                
