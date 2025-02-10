509a085f44d3932ff4517d34671ae8554fa66214
seungueonn <jeongsy8928@soongsil.ac.kr>
2025-02-10 15:04:37 +0900

    [BOJ] 바이러스 / 실버 3 / BFS

--- seungyeon/BAEKJOON/그래프 이론/2606_바이러스.py
+++ seungyeon/BAEKJOON/그래프 이론/2606_바이러스.py
@@ -1,84 +1,4 @@
 
-import sys
-from collections import deque
-input=sys.stdin.readline
-
-n=int(input())
-m=int(input())
-
-arr=[ [ ]for i in range(n+1)]
-visited=[False]*(n+1)
-
-for i in range(m):
-    a,b=map(int,input().split())
-    arr[a].append(b)
-    arr[b].append(a)
-
-answer_dfs = 0
-def dfs(k):
-
-    global answer_dfs
-    visited[k]= True
-    
-    for i in arr[k]:
-        if not visited[i]:
-            visited[i] = True
-            dfs(i)
-            answer_dfs+=1
-
-dfs(1)
-print(answer_dfs)
-
-
-
-# def bfs(k):
-
-#     answer = 0
-#     que = deque()
-
-#     que.append(k)
-
-#     while que:
-#         a = que.popleft()
-#         visited[a] = True
-
-#         for i in arr[a]:
-#             if not visited[i]:
-#                 que.append(i)
-#                 visited[i] = True
-#                 answer += 1
-
-#     return answer
-
-# print(bfs(1))
-
-# def get_parent(x):
-#     if parent[x] == x:
-#         return x
-#     parent[x] = get_parent(x)
-#     return parent[x]
-
-# def union_parent(a,b):
-#     a = get_parent(a)
-#     b = get_parent(b)
-
-#     if a < b:
-#         parent[b] = a
-#     else:
-#         parent[a] = b
-
-
-# def is_same_parent(a,b):
-#     return get_parent(a) == get_parent(b)
-
-# answer = 0
-# # 연결을 해두고 정점 순회하면서 union 하고 개수 세기    
-# for a,b in arr:
-#     if not is_same_parent(a,b):
-#         union_parent(a,b)
-#         answer += 1
-# print(answer)
-
 # import sys
 # from collections import deque
 # input=sys.stdin.readline
@@ -86,7 +6,7 @@ print(answer_dfs)
 # n=int(input())
 # m=int(input())
 
-# arr=[ [ ]for i in range(n+1)] # 리스트 컴프리헨션
+# arr=[ [ ]for i in range(n+1)]
 # visited=[False]*(n+1)
 
 # for i in range(m):
@@ -94,25 +14,138 @@ print(answer_dfs)
 #     arr[a].append(b)
 #     arr[b].append(a)
 
+# answer_dfs = 0
+# def dfs(k):
 
-# def bfs(k):
+#     global answer_dfs
+#     visited[k]= True
+    
+#     for i in arr[k]:
+#         if not visited[i]:
+#             visited[i] = True
+#             dfs(i)
+#             answer_dfs+=1
 
-#     answer = 0
-#     que = deque()
+# dfs(1)
+# print(answer_dfs)
 
-#     que.append(k)
 
-#     while que:
-#         a = que.popleft()
-#         visited[a] = True # 이거 안써서 틀렸었음
 
-#         for i in arr[a]:
-#             if not visited[i]:
-#                 que.append(i)
-#                 visited[i] = True
-#                 answer += 1
+# # def bfs(k):
 
-#     return answer
+# #     answer = 0
+# #     que = deque()
 
-# print(bfs(1))
+# #     que.append(k)
 
+# #     while que:
+# #         a = que.popleft()
+# #         visited[a] = True
+
+# #         for i in arr[a]:
+# #             if not visited[i]:
+# #                 que.append(i)
+# #                 visited[i] = True
+# #                 answer += 1
+
+# #     return answer
+
+# # print(bfs(1))
+
+# # def get_parent(x):
+# #     if parent[x] == x:
+# #         return x
+# #     parent[x] = get_parent(x)
+# #     return parent[x]
+
+# # def union_parent(a,b):
+# #     a = get_parent(a)
+# #     b = get_parent(b)
+
+# #     if a < b:
+# #         parent[b] = a
+# #     else:
+# #         parent[a] = b
+
+
+# # def is_same_parent(a,b):
+# #     return get_parent(a) == get_parent(b)
+
+# # answer = 0
+# # # 연결을 해두고 정점 순회하면서 union 하고 개수 세기    
+# # for a,b in arr:
+# #     if not is_same_parent(a,b):
+# #         union_parent(a,b)
+# #         answer += 1
+# # print(answer)
+
+# # import sys
+# # from collections import deque
+# # input=sys.stdin.readline
+
+# # n=int(input())
+# # m=int(input())
+
+# # arr=[ [ ]for i in range(n+1)] # 리스트 컴프리헨션
+# # visited=[False]*(n+1)
+
+# # for i in range(m):
+# #     a,b=map(int,input().split())
+# #     arr[a].append(b)
+# #     arr[b].append(a)
+
+
+# # def bfs(k):
+
+# #     answer = 0
+# #     que = deque()
+
+# #     que.append(k)
+
+# #     while que:
+# #         a = que.popleft()
+# #         visited[a] = True # 이거 안써서 틀렸었음
+
+# #         for i in arr[a]:
+# #             if not visited[i]:
+# #                 que.append(i)
+# #                 visited[i] = True
+# #                 answer += 1
+
+# #     return answer
+
+# # print(bfs(1))
+
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+n=int(input())
+m=int(input())
+
+visited=[False]*(n+1)
+arr=[[] for i in range(n+1)]
+for i in range(m):
+    a,b=map(int,input().split())
+    arr[a].append(b)
+    arr[b].append(a)
+
+def bfs(x):
+    cnt = 0
+    que=deque()
+    que.append(x)
+    visited[x] = True
+
+    while que:
+        x = que.popleft()
+ 
+ 
+        for i in arr[x]:
+            if not visited[i]:
+                visited[i] = True
+                que.append(i)
+                cnt += 1
+    
+    return cnt
+
+print(bfs(1))
