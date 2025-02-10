81c03f3fb5cfc9baa4eb26e17405177daf68abac
seungueonn <jeongsy8928@soongsil.ac.kr>
2025-02-10 15:05:25 +0900

    [BOJ] 암호 만들기 / 골드5 / 백트래킹

--- seungyeon/BAEKJOON/백트래킹/1759_암호 만들기.py
+++ seungyeon/BAEKJOON/백트래킹/1759_암호 만들기.py
@@ -1,34 +1,64 @@
-# 암호를 이루는 알파벳이 암호에서 증가하는 순서. abc (o) bac(x)
-# 최소 한개 모음 최소 두개 자음 
+# # 암호를 이루는 알파벳이 암호에서 증가하는 순서. abc (o) bac(x)
+# # 최소 한개 모음 최소 두개 자음 
+
+# import sys
+# from itertools import combinations
+
+# input=sys.stdin.readline
+
+# l,c=map(int,input().split())
+# arr=sorted(list(map(str,input().split())))
+# answer =[]
+
+# def backTracking(cnt,idx):
+#     if cnt == l:
+#         vo,co = 0,0
+        
+#         for i in range(l):
+#             if answer[i] in ['a','e','i','o','u']:
+#                 vo += 1
+#             else:
+#                 co += 1
+
+#         if vo >= 1 and co >= 2:
+#             print("".join(answer))
+#         return
+    
+#     for i in range(idx,c):
+#         answer.append(arr[i])
+#         backTracking(cnt+1, i+1)
+#         answer.pop()
+
+# backTracking(0,0)
+# # for i in combinations(arr,l):
+# #     print(''.join(i))
 
 import sys
-from itertools import combinations
-
 input=sys.stdin.readline
 
 l,c=map(int,input().split())
 arr=sorted(list(map(str,input().split())))
-answer =[]
+answer=[]
 
-def backTracking(cnt,idx):
-    if cnt == l:
-        vo,co = 0,0
-        
+# 정렬되어있는 모음1자음2
+
+def back(cnt,index):
+    if cnt == l: # 길이가 L이 되면 자음,모음 체크. 맞으면 print
+        vo,co=0,0
+
         for i in range(l):
-            if answer[i] in ['a','e','i','o','u']:
+            if answer[i] in  ['a','e','i','o','u']:
                 vo += 1
-            else:
+            else: 
                 co += 1
 
-        if vo >= 1 and co >= 2:
+        if vo >= 1 and co >=2 :
             print("".join(answer))
         return
     
-    for i in range(idx,c):
+    for i in range(index,c):
         answer.append(arr[i])
-        backTracking(cnt+1, i+1)
+        back(cnt+1,i+1)
         answer.pop()
 
-backTracking(0,0)
-# for i in combinations(arr,l):
-#     print(''.join(i))
+back(0,0)
