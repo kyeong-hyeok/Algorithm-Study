f1a0a5238540765a43cc3270425c420bc12c1f33
seungueonn <jeongsy8929@soongsil.ac.kr>
2025-03-13 22:09:53 +0900

    [BOJ] 0만들기 / 골드 5 / 재귀

--- seungyeon/BAEKJOON/재귀/7490_0만들기.py
+++ seungyeon/BAEKJOON/재귀/7490_0만들기.py
@@ -1,18 +1,42 @@
+# import sys
+# input=sys.stdin.readline
+
+# def recur(sum,sign,num,n,string):
+#     if (n == k):
+#         sum = sum + (sign*num)
+#         if sum == 0:
+#             print(string)
+#     else:
+#         recur(sum,sign,num*10+(n+1),n+1,string+' '+str(n+1))
+#         recur(sum+sign*num,1,(n+1),n+1,string+'+'+str(n+1))
+#         recur(sum+sign*num,-1,(n+1),n+1,string+'-'+str(n+1))
+
+# n=int(input())
+# for i in range(n):
+#     k = int(input())
+#     recur(0,1,1,1,"1")
+#     print()
+
+
+
 import sys
 input=sys.stdin.readline
 
-def recur(sum,sign,num,n,string):
-    if (n == k):
+def recur(sum, index, sign, string, num ):
+
+    if (index == k):
         sum = sum + (sign*num)
-        if sum == 0:
+        if (sum == 0):
             print(string)
     else:
-        recur(sum,sign,num*10+(n+1),n+1,string+' '+str(n+1))
-        recur(sum+sign*num,1,(n+1),n+1,string+'+'+str(n+1))
-        recur(sum+sign*num,-1,(n+1),n+1,string+'-'+str(n+1))
+        recur(sum+num*sign, index+1,1,string+"+"+ str(index+1),index+1)
+        recur(sum+num*sign, index+1,-1,string+"-"+str(index+1),index+1)
+        recur(sum, index+1 ,sign,string+" "+str(index+1), num*10 + index + 1)
 
 n=int(input())
 for i in range(n):
     k = int(input())
-    recur(0,1,1,1,"1")
-    print()
+
+    recur(0,1,1,"1",1)
+
+
