e45fe2409a9f6aa78c3c68c7877f226c8d7ab354
seungueonn <jeongsy8928@soongsil.ac.kr>
2025-03-20 17:20:39 +0900

    [BOJ] 구간합 구하기 / 투포인터/ 실버

--- seungyeon/BAEKJOON/투포인터/11659_구간 합 구하기 4.py
+++ seungyeon/BAEKJOON/투포인터/11659_구간 합 구하기 4.py
@@ -1,11 +1,25 @@
+# import sys
+# input = sys.stdin.readline
+
+# n,m=map(int,input().split())
+# num = [0]+ list(map(int,input().split()))
+# for i in range(len(num)-1):
+#     num[i+1] += num[i]
+    
+# for i in range(m):
+#     x,y=map(int,input().split())
+#     print(num[y]-num[x-1])
+
+
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
 n,m=map(int,input().split())
-num = [0]+ list(map(int,input().split()))
-for i in range(len(num)-1):
-    num[i+1] += num[i]
-    
+arr=[0] + list(map(int,input().split()))
+for i in range(n):
+    arr[i+1] += arr[i]
+
 for i in range(m):
-    x,y=map(int,input().split())
-    print(num[y]-num[x-1])
+    a,b=map(int,input().split())
+    print(arr[b]-arr[a-1])
+
