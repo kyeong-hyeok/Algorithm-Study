36a99608ab72a9d814a23c299d29cd257188f039
seungueonn <jeongsy8928@soongsil.ac.kr>
2025-04-15 10:05:56 +0900

    [BOJ] 벽부수고 이동하기 / BFS / 골드 2

--- seungyeon/BAEKJOON/BFS/2206_벽부수고 이동하기.py
+++ seungyeon/BAEKJOON/BFS/2206_벽부수고 이동하기.py
@@ -1,45 +1,96 @@
-# import sys
-# from collections import deque
-# input=sys.stdin.readline
+# # import sys
+# # from collections import deque
+# # input=sys.stdin.readline
 
-# n,m=map(int,input().split(" "))
-# arr=[]
-# for i in range(n):
-#     arr.append(list(map(int,input().rstrip())))
+# # n,m=map(int,input().split(" "))
+# # arr=[]
+# # for i in range(n):
+# #     arr.append(list(map(int,input().rstrip())))
 
-# visited=[[[0] * 2 for _ in range(m)]]
+# # visited=[[[0] * 2 for _ in range(m)]]
 
-# dx=[-1,1,0,0]
-# dy=[0,0,1,-1]
+# # dx=[-1,1,0,0]
+# # dy=[0,0,1,-1]
 
-# def bfs(x,y,z):
+# # def bfs(x,y,z):
 
-#     que=deque()
-#     que.append((x,y,z))
+# #     que=deque()
+# #     que.append((x,y,z))
 
-#     while que:
+# #     while que:
 
-#         if x == m-1 and y == n-1:
-#             return visited[x][y][z]
+# #         if x == m-1 and y == n-1:
+# #             return visited[x][y][z]
         
-#         for i in range(4):
-#             nx=x+dx[i]
-#             ny=y+dy[i]
+# #         for i in range(4):
+# #             nx=x+dx[i]
+# #             ny=y+dy[i]
 
-#             if nx < 0 or ny < 0 or nx >= m or ny >= n:
-#                 continue
+# #             if nx < 0 or ny < 0 or nx >= m or ny >= n:
+# #                 continue
 
 
-#             if arr[nx][ny] == 1 and z == 0 :
-#                 visited[nx][ny][1] = visited[x][y][0] + 1
-#                 que.append((nx,ny,1))
-#             elif arr[nx][ny] == 0 and visited[nx][ny][z] == 0:
-#                 visited[nx][ny][z] = visited[x][y][z] + 1
-#                 que.append((nx,ny,z))
+# #             if arr[nx][ny] == 1 and z == 0 :
+# #                 visited[nx][ny][1] = visited[x][y][0] + 1
+# #                 que.append((nx,ny,1))
+# #             elif arr[nx][ny] == 0 and visited[nx][ny][z] == 0:
+# #                 visited[nx][ny][z] = visited[x][y][z] + 1
+# #                 que.append((nx,ny,z))
 
-#     return -1
+# #     return -1
 
-# print((bfs(0,0,0)))
+# # print((bfs(0,0,0)))
+
+# # import sys
+# # from collections import deque
+
+# # input=sys.stdin.readline
+
+# # n,m=map(int,input().split(" "))
+
+# # arr=[list(map(int,input().strip())) for _ in range(n)]
+
+
+# # dx=[-1,1,0,0]
+# # dy=[0,0,1,-1]
+
+
+# # def bfs(x,y):
+
+# #     arr[x][y] = 1
+# #     que=deque()
+# #     que.append((x,y))
+
+# #     while que:
+# #         x,y=que.popleft()
+
+# #         for i in range(4):
+# #             nx=x+dx[i]
+# #             ny=y+dy[i]
+
+# #             if nx < 0 or ny < 0 or nx >= n or ny >= m:
+# #                 continue
+
+# #             if arr[nx][ny] == 0:
+# #                 arr[nx][ny] = arr[x][y] + 1
+# #                 que.append((nx,ny))
+
+# #     return arr[n-1][m-1]
+
+# # answer = 222222
+
+# #  # 한개만 부셔서 최단거리 
+# # for i in range(n):
+# #     for j in range(m):
+# #         if arr[i][j] == 1:
+# #             arr[i][j] = 0 
+# #             answer = min(answer,bfs(0,0))
+# #             arr[i][j] = 1
+
+# # if answer == 0:
+# #     print(-1)
+# # else : print(answer)
+
 
 # import sys
 # from collections import deque
@@ -47,6 +98,10 @@
 # input=sys.stdin.readline
 
 # n,m=map(int,input().split(" "))
+
+# visited=[[[0] * 2 for _ in range(m)] for _ in range(n)]
+# visited[0][0][0] = 1
+
 
 # arr=[list(map(int,input().strip())) for _ in range(n)]
 
@@ -55,14 +110,17 @@
 # dy=[0,0,1,-1]
 
 
-# def bfs(x,y):
+# def bfs(x,y,z):
 
-#     arr[x][y] = 1
 #     que=deque()
-#     que.append((x,y))
+#     que.append((x,y,z))
 
 #     while que:
-#         x,y=que.popleft()
+#         x,y,z=que.popleft()
+
+#         if x == n-1 and y == m-1:
+#             return visited[x][y][z]
+        
 
 #         for i in range(4):
 #             nx=x+dx[i]
@@ -71,48 +129,34 @@
 #             if nx < 0 or ny < 0 or nx >= n or ny >= m:
 #                 continue
 
-#             if arr[nx][ny] == 0:
-#                 arr[nx][ny] = arr[x][y] + 1
-#                 que.append((nx,ny))
+#             if arr[nx][ny] == 1 and z == 0:
+#                 visited[nx][ny][1] = visited[x][y][0] + 1
+#                 que.append((nx,ny,1))
 
-#     return arr[n-1][m-1]
-
-# answer = 222222
-
-#  # 한개만 부셔서 최단거리 
-# for i in range(n):
-#     for j in range(m):
-#         if arr[i][j] == 1:
-#             arr[i][j] = 0 
-#             answer = min(answer,bfs(0,0))
-#             arr[i][j] = 1
-
-# if answer == 0:
-#     print(-1)
-# else : print(answer)
+#             elif arr[nx][ny] == 0 and visited[nx][ny][z] == 0:
+#                 visited[nx][ny][z] = visited[x][y][z]+ 1
+#                 que.append((nx,ny,z))
 
 
-import sys
+#     return -1
+
+# print(bfs(0,0,0))
+
 from collections import deque
 
-input=sys.stdin.readline
+n,m=map(int,input().split())
 
-n,m=map(int,input().split(" "))
-
-visited=[[[0] * 2 for _ in range(m)] for _ in range(n)]
-visited[0][0][0] = 1
-
-
-arr=[list(map(int,input().strip())) for _ in range(n)]
-
+arr=[]
+for i in range(n):
+    arr.append((list(map(int,input().strip()))))
 
 dx=[-1,1,0,0]
 dy=[0,0,1,-1]
 
-
+que=deque()
+visited=[[[0] * 2 for _ in range(m)] for _ in range(n)]
+visited[0][0][0] = 1
 def bfs(x,y,z):
-
-    que=deque()
     que.append((x,y,z))
 
     while que:
@@ -120,24 +164,23 @@ def bfs(x,y,z):
 
         if x == n-1 and y == m-1:
             return visited[x][y][z]
-        
 
         for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
 
             if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
 
-            if arr[nx][ny] == 1 and z == 0:
+            # 벽을 부수지 않고 이동 
+            if arr[nx][ny] == 0 and visited[nx][ny][z] == 0:
+                visited[nx][ny][z] = visited[x][y][z] + 1
+                que.append((nx,ny,z))
+            
+            # 벽을 부수고 이동 
+            elif arr[nx][ny] == 1 and z == 0:
                 visited[nx][ny][1] = visited[x][y][0] + 1
                 que.append((nx,ny,1))
-
-            elif arr[nx][ny] == 0 and visited[nx][ny][z] == 0:
-                visited[nx][ny][z] = visited[x][y][z]+ 1
-                que.append((nx,ny,z))
-
-
     return -1
 
 print(bfs(0,0,0))
